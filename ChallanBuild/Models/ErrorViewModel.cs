493019ba3a7a28d422642e788c5087d0fb93dba0
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2024-03-25 18:14:52 +0200

    Add project files.

---
+++ ChallanBuild/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChallanBuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
